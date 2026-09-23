41577a18aad3dabfca7bf3fd50b263d0476d6aa4
yubayu0 <yubayu2004@gmail.com>
2026-09-23 17:43:19 +0700

    fix production build and deployment

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,12 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/react-start/plugin/vite'
-import viteReact from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import { tanstackStart } from "@tanstack/react-start/plugin/vite";
+import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  server: { port: 3000 },
-  resolve: { tsconfigPaths: true },
-  plugins: [tanstackStart(), viteReact(), tailwindcss()],
-})
+  plugins: [
+    tanstackStart(),
+    react(),
+    tailwindcss(),
+  ],
+});
